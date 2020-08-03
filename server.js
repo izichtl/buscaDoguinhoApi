b01318414979ea0f86328b5b0ebb6142318076b3
izichtl <izichtl@gmail.com>
2020-08-03 04:00:30 -0300

    a

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 //Create a express server
 const app = require('./app')
-const favicon = require('express-favicon')
+//const favicon = require('express-favicon')
 const PORT = process.env.PORT || 8080;
 
 
-app.use(favicon(__dirname + '/client/build/favicon.ico'));
+//app.use(favicon(__dirname + '/client/build/favicon.ico'));
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
